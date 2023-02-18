5db7c066801a636c0d03a9384313f6b8f156f145
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-02-18 10:37:28 +0500

    fix wrong paths in commands

--- bin/abi.js
+++ bin/abi.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-require('../lib/abi/run')
+require('../lib/commands/abi')
--- bin/config.js
+++ bin/config.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-require('../lib/config/run')
+require('../lib/commands/config')
