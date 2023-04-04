5b3582ddbb3c8e1c3910c92f12a42e952bc5ca7d
Anton Bernatskiy <abernatskiy@toha.li>
2023-04-05 04:56:46 +0900

    Help fixes (#29)

--- squid-gen-ink/src/commands/config.ts
+++ squid-gen-ink/src/commands/config.ts
@@ -11,41 +11,21 @@ import fs from 'fs'
 program
     .addHelpText(
         'before',
-        'Generates code for squid that can index specific ink events for each provided contract. Run within a folder cloned from https://github.com/subsquid/squid-wasm-abi-template.\n'
+        'Generates code for a squid indexing specific ink! events for each provided contract. Run within a folder cloned from https://github.com/subsquid-labs/squid-ink-abi-template.\n'
     )
-    .argument('<config>', '(required) A JSON configuration file. See the full schema below.')
+    .argument('<config>', '(required) A YAML configuration file.')
     .addHelpText(
         'after',
-        '\nFull schema (TypeScript-like):\n' +
-            '{\n' +
-            '    archive: string; // Source Squid Archive for a network. Can be a URL or\n' +
-            '                     // an alias. See https://docs.subsquid.io/archives/overview\n' +
-            '    contracts: ContractJSON[]; // ContractJSONs defining the exact data to\n' +
-            '                               // extract from each contract. See details below.\n' +
-            '}\nwhere\n' +
-            'ContractJSON {\n' +
-            '    name: string; // Contract string name.\n' +
-            '    address: string; // Contract address.\n' +
-            '    abi: string; // (optional) Path to the contract JSON ABI.\n' +
-            '    events?: string[] | boolean; // One or multiple contract events to be\n' +
-            '                                 // indexed. true indexes all events defined in\n' +
-            '                                 // the ABI; false indexes none. Default: false\n' +
-            '    range?: { // Range of blocks to index. Default: all from the genesis.\n' +
-            '        from?: number;\n' +
-            '        to?: number;\n' +
-            '    };\n' +
-            '}\n\n' +
-            'Example:\n' +
-            '{\n' +
-            '    "archive": "shibuya",\n' +
-            '    "contracts": [\n' +
-            '        {\n' +
-            '            "name": "erc20",\n' +
-            '            "address": "0x5207202c27b646ceeb294ce516d4334edafbd771f869215cb070ba51dd7e2c72",\n' +
-            '            "events": true,\n' +
-            '        }\n' +
-            '    ]\n' +
-            '}\n'
+        '\nExample config:\n\n' +
+        'archive: shibuya\n' +
+        'target:\n' +
+        '  type: postgres\n' +
+        'contracts:\n' +
+        '  - name: erc20\n' +
+        '    abi: ./abi/erc20.json\n' +
+        '    address: "0x5207202c27b646ceeb294ce516d4334edafbd771f869215cb070ba51dd7e2c72"\n' +
+        '    events: true\n\n' +
+        'See the full configuration file schema at node_modules/@subsquid/squid-gen-ink/lib/config.schema.json (JSON schema) or at node_modules/@subsquid/squid-gen-ink/lib/config.d.ts (Typescript).'
     )
 
 runProgram(async function () {
